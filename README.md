2198af2b8849c0ee8da2abe10e9c0e39a9be845a
Kevin Matheus Martins <kevin.martins@mercadolivre.com>
2024-04-22 11:03:19 -0300

    Implement dockerfile

---
+++ README.md
@@ -0,0 +1,32 @@
+# Tech Challenge
+
+## Install
+
+### Go 
+
+- [Go Instalation](https://go.dev/doc/install)
+
+> Make sure you have Go 1.22.2 or higher
+
+### Docker compose
+
+```shell
+docker-compose build
+
+docker-compose up -d
+
+curl --request GET --url http://localhost:8080/status
+
+## Expected response
+{
+"status": "ok"
+}
+```
+
+## Stack
+
+- [Go](https://go.dev/)
+- [Gin Web Framework](https://gin-gonic.com/) - Routes, JSON validation, Error management, Middleware support
+- [PostgreSQL](https://www.postgresql.org/) - Database
+
+
--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -1,26 +1,41 @@
 package main
 
 import (
+	"fmt"
 	db2 "github.com/pangolin-do-golang/tech-challenge/internal/adapters/db"
 	"github.com/pangolin-do-golang/tech-challenge/internal/adapters/rest/server"
 	"github.com/pangolin-do-golang/tech-challenge/internal/application/order"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
+	"time"
 )
 
 func main() {
-	dsn := "host=localhost user=user password=pass dbname=postgres port=5432 sslmode=disable TimeZone=America/Sao_Paulo"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := initDb()
+	if err != nil {
+		panic(err)
+	}
+	orderRepository := db2.NewPostgresOrderRepository(db)
+	orderService := order.NewOrderService(orderRepository)
+	restServer := server.NewRestServer(orderService)
+	restServer.Serve()
+}
 
+func initDb() (*gorm.DB, error) {
+	loc, _ := time.LoadLocation("America/Sao_Paulo")
+	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable TimeZone=%s",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+		loc,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Panic(err)
 	}
-
-	orderRepository := db2.NewPostgresOrderRepository(db)
-	orderService := order.NewOrderService(orderRepository)
-
-	restServer := server.NewRestServer(orderService)
-
-	restServer.Serve()
+	return db, nil
 }
